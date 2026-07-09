d41b36589f60458e8a500ddae719f0770d88248a
shray77 <282795193+shray77@users.noreply.github.com>
2026-07-09 23:41:34 +0300

    Починить CycloneDDS XML-схему под реально установленную версию 0.7.0
    Скрипты генерировали <Interfaces><NetworkInterface .../></Interfaces> —
    эта вложенная схема появилась в CycloneDDS 0.8+. На роботе реально стоит
    ros-foxy-cyclonedds 0.7.0, которая с этой схемой падает: "Interfaces:
    unknown element". Заменено на старую плоскую <NetworkInterfaceAddress>,
    совместимую с 0.7.0. Подтверждено на живом роботе (dpkg -l | grep cyclonedds).

--- ros2_g1_cup_solution/scripts/env_g1_motion_foxy.sh
+++ ros2_g1_cup_solution/scripts/env_g1_motion_foxy.sh
@@ -7,11 +7,15 @@ ROBOT_IP="${UNITREE_ROBOT_IP:-192.168.123.161}"
 IFACE="$(ip route get "$ROBOT_IP" 2>/dev/null | awk '{for(i=1;i<=NF;i++){if($i=="dev"){print $(i+1); exit}}}')"
 if [ -z "$IFACE" ]; then IFACE="${UNITREE_NET_IFACE:-eth0}"; fi
 export UNITREE_NET_IFACE="$IFACE"
+# CycloneDDS 0.7.0 (реально установлено на этом роботе, ros-foxy-cyclonedds
+# 0.7.0-1focal) использует СТАРУЮ плоскую схему <NetworkInterfaceAddress> —
+# вложенный <Interfaces><NetworkInterface .../></Interfaces> появился только
+# в 0.8+ и на 0.7.0 падает с "unknown element". Проверено на живом роботе.
 cat > /tmp/cyclonedds_g1_motion.xml <<XML
 <CycloneDDS>
-  <Domain id="any">
+  <Domain>
     <General>
-      <Interfaces><NetworkInterface name="$IFACE" multicast="true"/></Interfaces>
+      <NetworkInterfaceAddress>$IFACE</NetworkInterfaceAddress>
       <AllowMulticast>true</AllowMulticast>
     </General>
   </Domain>
--- ros2_g1_cup_solution/scripts/env_sensor_foxy.sh
+++ ros2_g1_cup_solution/scripts/env_sensor_foxy.sh
@@ -3,11 +3,13 @@ if [ -f /opt/ros/foxy/setup.bash ]; then source /opt/ros/foxy/setup.bash; fi
 if [ -f ~/g1_foxy_ws/install/setup.bash ]; then source ~/g1_foxy_ws/install/setup.bash; fi
 export ROS_DOMAIN_ID="${ROS_DOMAIN_ID:-77}"
 export RMW_IMPLEMENTATION="${RMW_IMPLEMENTATION:-rmw_cyclonedds_cpp}"
+# CycloneDDS 0.7.0 (реально установлено) требует старую плоскую схему
+# <NetworkInterfaceAddress> — вложенный <Interfaces> появился в 0.8+.
 cat > /tmp/cyclonedds_lo.xml <<'XML'
 <CycloneDDS>
-  <Domain id="any">
+  <Domain>
     <General>
-      <Interfaces><NetworkInterface name="lo" multicast="false"/></Interfaces>
+      <NetworkInterfaceAddress>lo</NetworkInterfaceAddress>
       <AllowMulticast>false</AllowMulticast>
     </General>
   </Domain>
